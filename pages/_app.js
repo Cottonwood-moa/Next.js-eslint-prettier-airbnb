f2da7127d1e840eae3b3e6b5c37cd172eb2e871e
Cottonwood <blog_geon@naver.com>
2022-02-12 01:09:10 +0900

    add layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,9 @@
-import NavBar from '../components/NavBar';
 import '../styles/globals.css';
-
+import Layout from '../components/Layout';
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
-      <Component {...pageProps} />;
-      <style jsx global>
-        {`
-          a {
-            color: royalblue;
-          }
-        `}
-      </style>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 const index = () => {
   return (
     <>
-      <div>
-        <h1>Hello</h1>
+      <div className="container">
+        <h1 className="hello">Hello</h1>
       </div>
       <style jsx>
         {`
-          div {
-            h1 {
-              color: tomato;
+          .container {
+            .hello {
+              color: red;
             }
           }
         `}
